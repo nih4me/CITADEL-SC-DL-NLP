22c3ce2db64acddba37e2a60b20e4201d796ea41
Abdoul Kader <derokabore@gmail.com>
2022-09-17 20:01:56 +0200

    init

---
+++ NLP/download.py
@@ -0,0 +1,6 @@
+from transformers import CamembertModel, CamembertTokenizer
+
+tokenizer = 
+CamembertTokenizer.from_pretrained("camembert/camembert-base-wikipedia-4gb")
+camembert = 
+CamembertModel.from_pretrained("camembert/camembert-base-wikipedia-4gb")
