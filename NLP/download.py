3dab6633faee92095e444d23e8a86cc3b23e5221
Abdoul Kader <derokabore@gmail.com>
2022-09-19 11:03:21 +0200

    Update download.py

--- NLP/download.py
+++ NLP/download.py
@@ -1,6 +1,7 @@
 from transformers import CamembertModel, CamembertTokenizer
+import spacy
 
-tokenizer = 
-CamembertTokenizer.from_pretrained("camembert/camembert-base-wikipedia-4gb")
-camembert = 
-CamembertModel.from_pretrained("camembert/camembert-base-wikipedia-4gb")
+tokenizer = CamembertTokenizer.from_pretrained("camembert/camembert-base-wikipedia-4gb")
+camembert = CamembertModel.from_pretrained("camembert/camembert-base-wikipedia-4gb")
+
+nlp = spacy.load("fr_core_news_sm")
