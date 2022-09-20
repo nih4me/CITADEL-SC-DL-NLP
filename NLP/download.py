661cf73777020ef37755dde388db071ce454ca9d
Abdoul Kader <derokabore@gmail.com>
2022-09-20 15:15:19 +0200

    add stopwords download

--- NLP/download.py
+++ NLP/download.py
@@ -1,7 +1,10 @@
 from transformers import CamembertModel, CamembertTokenizer
 import spacy
+import nltk
 
 tokenizer = CamembertTokenizer.from_pretrained("camembert/camembert-base-wikipedia-4gb")
 camembert = CamembertModel.from_pretrained("camembert/camembert-base-wikipedia-4gb")
 
 nlp = spacy.load("fr_core_news_sm")
+
+nltk.download('stopwords')
